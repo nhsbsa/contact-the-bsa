98fe2e60da3aea3606bda9de1dc2ba2c2f03c282
katie.twinn@nhsbsa.nhs.uk <katie.twinn@nhsbsa.nhs.uk>
2024-06-12 09:34:20 +0100

    Removing XML and XLSM from upload types

--- app/routes.js
+++ app/routes.js
@@ -10,6 +10,28 @@ router.post('/clear-data', function (req, res) {
     res.redirect('/')
 })
 
+// log stuff - thanks Craig Abbott!
+
+router.use((req, res, next) => {
+    const log = {
+        method: req.method,
+        url: req.originalUrl, //URL of page
+        data: req.session.data //all data held
+    }
+    console.log(JSON.stringify(log, null, 2)) // show all data as a dump in terminal
+    next() // continue to next action
+})
+
+// GET FOLDER NAME - useful for relative templates
+router.use('/', (req, res, next) => {
+    req.folder = req.originalUrl.split('/')[1]; //folder, e.g. 'current'
+    req.subfolder = req.originalUrl.split('/')[2]; //sub-folder e.g. 'service'
+    res.locals.folder = req.folder; // what folder the url is
+    res.locals.subfolder = req.subfolder; // what subfolder the URL is in
+    console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder);
+    next();
+});
+
 // ****************************************
 // Route File Versions
 // ****************************************
