90c44e1e25b9a42616a82370a793d9271a112dcb
katie.twinn@nhsbsa.nhs.uk <katie.twinn@nhsbsa.nhs.uk>
2025-05-21 07:17:05 +0100

    updating the prototype-kit to 6.0

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'NHS.UK prototype kit',
+  serviceName: 'Contact the NHS Business Services Authority (NHSBSA)',
 
   // Port to run nodemon on locally
   port: 2000,
